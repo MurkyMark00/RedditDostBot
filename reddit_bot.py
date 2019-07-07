5fa837c044cfc58723e56254052875522d11b078
MurkyMark00 <murkymarkymark@gmail.com>
2019-07-07 20:32:03 +0300

    fixed a major bug

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,9 +1,11 @@
 import praw
 import twitter_link
+from os import path
 
+directory_path = path.dirname(path.realpath(__file__))
 
 tweet_id = twitter_link.get_id()
-with open("tweet_id.txt", "r") as f:
+with open(f"{directory_path}/tweet_id.txt", "r") as f:
     # If it already posted the same id
     if f.readline() == tweet_id:
         with open("temp.txt", "a") as g:
@@ -16,5 +18,5 @@ with open("tweet_id.txt", "r") as f:
         subreddit.submit(
             twitter_link.get_title(tweet_id), url=f"https://twitter.com/DostKayaoglu/status/{tweet_id}").mod.distinguish(sticky=True)
 
-        with open("tweet_id.txt", "w") as f:
+        with open(f"{directory_path}/tweet_id.txt", "w") as f:
             f.write(tweet_id)
